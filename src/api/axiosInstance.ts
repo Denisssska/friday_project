02ac023a9ac398d7cb94362805de70f54c4b5ded
Denisssska <yarmoshkoden18m@gmail.com>
2022-10-28 02:56:09 +0300

    update

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -2,10 +2,11 @@ import axios from "axios";
 
 export const instance = axios.create({
     withCredentials: true,
-      baseURL: process.env.REACT_APP_BACK_URL || 'http://localhost:7542/2.0/'
-    // baseURL: 'https://neko-back.herokuapp.com/2.0/'
+    //baseURL: process.env.REACT_APP_BACK_URL || 'http://localhost:7542/2.0/'
+
+    baseURL: process.env.REACT_APP_BACK_URL || 'https://neko-back.herokuapp.com/2.0/'
 });
-export const instance2 = axios.create({
-    withCredentials:true,baseURL:'https://dry-forest-56016.herokuapp.com/'
-}
-)
+// export const instance2 = axios.create({
+//     withCredentials:true,baseURL:'https://dry-forest-56016.herokuapp.com/'
+// }
+// )
--- src/api/changePasswordAPI.ts
+++ src/api/changePasswordAPI.ts
@@ -7,7 +7,7 @@ export const changePassword = {
             email,
             message: `<div style="background-color: lime; padding: 15px">
                         password recovery link: 
-                        <a href='http://localhost:3000/create-password/$token$'>link</a>
+                        <a href='https://Denisssska.github.io/friday_project/create-password/$token$'>link</a>
                       </div>`
         });
     },
